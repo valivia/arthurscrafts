c36cb8d01a37b07b9108647748b37ffc6c50471c
valivia <26413018+valivia@users.noreply.github.com>
2024-12-10 15:29:17 +0000

    ✨ Added proper gallery

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,8 +1,20 @@
-<script>
+<script lang="ts">
     import Header from "components/models/Header.svelte";
     import Gallery from "components/models/Gallery.svelte";
 
     let { data } = $props();
+
+    let categories = data.categories;
+    let currentCategory: string | null = $state(null);
+
+    let images = $derived.by(() => {
+        if (currentCategory === null)
+            return categories.flatMap((category) => category.images);
+        return (
+            categories.find((category) => category.name === currentCategory)
+                ?.images ?? []
+        );
+    });
 </script>
 
 <svelte:head>
@@ -14,12 +26,29 @@
 </Header>
 
 <main>
-    {#each data.categories as category}
-        <h2>{category.name}</h2>
-        <Gallery images={category.images} sizeMultiplier={1} />
-    {:else}
-        <p>No images yet</p>
-    {/each}
+    <nav class="categorySelect">
+        <button
+            onclick={() => (currentCategory = null)}
+            class:active={currentCategory === null}
+        >
+            All
+        </button>
+        {#each categories as category}
+            <button
+                onclick={() => (currentCategory = category.name)}
+                class:active={currentCategory === category.name}
+            >
+                {category.name}
+            </button>
+        {/each}
+    </nav>
+    <div class="content">
+        {#if images.length === 0}
+            <p>No images found.</p>
+        {:else}
+            <Gallery {images} sizeMultiplier={1} />
+        {/if}
+    </div>
 </main>
 
 <style lang="scss">
@@ -31,5 +60,37 @@
         p {
             text-align: center;
         }
+    }
+
+    .categorySelect {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin-bottom: 1rem;
+
+        & .active {
+            color: var(--theme-accent);
+            border-bottom: 2px solid currentColor;
+        }
+
+        button {
+            padding-inline: 1rem;
+            padding-block: 0.8rem 0.2rem;
+            border: none;
+            border-radius: 0;
+            background-color: var(--color-primary);
+            cursor: pointer;
+            transition: background-color 0.2s;
+
+            text-transform: capitalize;
+
+            &:hover {
+                background-color: var(--color-primary-dark);
+            }
+        }
+    }
+
+    .content {
+        min-height: 60dvh;
     }
 </style>
